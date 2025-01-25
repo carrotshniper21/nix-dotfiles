6c8f6d1201214064e6b359eabacc71bed5095f37
Jason Spalti <vipershniper07@gmail.com>
2025-01-25 21:26:13 +0000

    Update Dotfiles

--- home.nix
+++ home.nix
@@ -6,21 +6,21 @@
   imports = [
     ./rofi
     ./wezterm
-    ./picom
-    ./bin/dwm_battery.nix
-    ./bin/dwm_brightness.nix
-    ./bin/dwm_clock.nix
-    ./bin/dwm_cpu_temp.nix
-    ./bin/dwm_date.nix
-    ./bin/dwm_kernel.nix
-    ./bin/dwm_keyboard_layout.nix
-    ./bin/dwm_memory.nix
-    ./bin/dwm_network.nix
-    ./bin/dwm_traffic.nix
-    ./bin/dwm_uptime.nix
-    ./bin/dwm_volume.nix
-    ./bin/set_resolution.nix
-  ];
+    ./picom.nix
+    # ./bin/dwm_battery.nix
+    # ./bin/dwm_brightness.nix
+    # ./bin/dwm_clock.nix
+    # ./bin/dwm_cpu_temp.nix
+    # ./bin/dwm_date.nix
+    # ./bin/dwm_kernel.nix
+    # ./bin/dwm_keyboard_layout.nix
+    # ./bin/dwm_memory.nix
+    # ./bin/dwm_network.nix
+    # ./bin/dwm_traffic.nix
+    # ./bin/dwm_uptime.nix
+    # ./bin/dwm_volume.nix
+    # ./bin/set_resolution.nix
+  ]; 
 
   home.packages = with pkgs; [
     pulseaudio
